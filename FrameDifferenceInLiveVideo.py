8daa6a5603414852c15e36cb062c51c32462e46a
Vijay P V <52239002+hack-e-d@users.noreply.github.com>
2019-07-17 22:18:32 +0530

    Add files via upload
    The code finds the difference between two consecutive video frames

---
+++ FrameDifferenceInLiveVideo.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+import time
+# Capture video from file
+cap=cv2.VideoCapture(0)
+
+old_frame = None
+
+while True:
+
+    ret, frame = cap.read()
+    #Goes in the loop only if the frame is returned ret is a return function
+    if ret == True:
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #Only if the previous frame is present the difference is found
+        if old_frame is not None:
+            #The frame difference is found
+            diff_frame = gray - old_frame
+            diff_frame -= diff_frame.min()
+            #The difference is calculated is an array value which is converted back to a video frame for displaying
+            disp_frame = np.uint8(255.0*diff_frame/float(diff_frame.max()))
+            cv2.imshow('diff_frame',disp_frame)
+            #Time delay between refreshing of frames is done every two seconds
+            time.sleep(2)
+        old_frame = gray
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    else:
+        print('ERROR!')
+        break
+
+cap.release()
+cv2.destroyAllWindows()
